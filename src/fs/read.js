681c05da7f45bdfe0da2bf364a1a6d3c9d5f5aa2
Yury_Viarenich <verenichyi@gmail.com>
2024-10-05 12:47:44 +0300

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { readFile } from 'node:fs/promises';
+import { checkExistence } from './utils.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const file = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const isFileExist = await checkExistence(file, 'file');
+
+    if (!isFileExist) {
+        throw new Error('FS operation failed');
+    }
+
+    const content = await readFile(file, 'utf-8');
+    console.log(content);
 };
 
 await read();
