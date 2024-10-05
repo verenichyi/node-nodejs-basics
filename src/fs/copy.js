7c9756fced91a689a32e1dbf96aba609cf99a97e
Yury_Viarenich <verenichyi@gmail.com>
2024-10-05 12:30:57 +0300

    feat: implement copy.js, util func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { checkExistence, copyDirectory } from './utils.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const src = join(__dirname, 'files');
+const dest = join(__dirname, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    const isSrcExist = await checkExistence(src, 'directory');
+    const isDestExist = await checkExistence(dest, 'directory');
+
+    if (!isSrcExist || isDestExist) {
+        throw new Error('FS operation failed');
+    }
+
+    await copyDirectory(src, dest);
 };
 
 await copy();
--- src/fs/utils.js
+++ src/fs/utils.js
@@ -1,4 +1,5 @@
-import { stat } from 'node:fs/promises';
+import { copyFile, mkdir, readdir, stat } from 'node:fs/promises';
+import { join } from 'node:path';
 
 export const checkExistence = async (path, entity) => {
 	try {
@@ -12,5 +13,22 @@ export const checkExistence = async (path, entity) => {
 		}
 	} catch (error) {
 		 return error.code !== 'ENOENT';
+	}
+}
+
+export const copyDirectory = async (src, dest) => {
+	const entities = await readdir(src);
+	await mkdir(dest);
+
+	for (const entity of entities) {
+		const entitySourcePath = join(src, entity);
+		const stats = await stat(entitySourcePath)
+
+		if (stats.isFile()) {
+			const entityDestPath = join(dest, entity);
+			await copyFile(entitySourcePath, entityDestPath)
+		} else if (stats.isDirectory()) {
+			await copyDirectory(join(src, entity), join(dest, entity));
+		}
 	}
 }
